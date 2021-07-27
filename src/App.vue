5a7200a55062e2d64ccfd3fc9ce8443a5b898838
dingchaorong <dingchaorong@zuoyebang.com>
2021-07-27 11:32:06 +0800

    uopdate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App1233"/>
+    <HelloWorld msg="Welcome to Your Vue.js App123344"/>
   </div>
 </template>
 
